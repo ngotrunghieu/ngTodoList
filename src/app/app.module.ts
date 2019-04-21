2fee8595c79b076c81140de703acf8586b4ce7dd
ngotrunghieu <hieuit12qn@gmail.com>
2019-04-21 09:11:48 +0700

    update code 21-apr-2019

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { TodoListComponent } from './todoList/todoList.component';
+import { AppRoutingModule } from './app-routing.module';
+import { TodoListService } from './todo-list.service';
+import { TodoDetailComponent } from './todo-detail/todo-detail.component';
+import { TodoHeaderComponent } from './todo-header/todo-header.component';
+import { TodoContentComponent } from './todo-content/todo-content.component';
+import { TodoNavPageComponent } from './todo-nav-page/todo-nav-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoListComponent,
+    TodoDetailComponent,
+    TodoHeaderComponent,
+    TodoContentComponent,
+    TodoNavPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [
+    TodoListService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
